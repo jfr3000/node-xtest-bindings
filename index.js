6396cb9c8821abe8dd8e2cef99841d0cf855e011
Julia Friesel <julia.friesel@gmail.com>
2016-01-15 15:35:44 +0100

    restructure exported object

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 var NativeExtension = require('bindings')('NativeExtension');
-module.exports = NativeExtension;
-module.exports.keySyms = require('./keysymdef');
+module.exports = function(display) {
+    var ret = NativeExtension.MyObject(display);
+    ret.keySyms = require('./keysymdef');
+    return ret;
+};
