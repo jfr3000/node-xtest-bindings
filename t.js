04afa4661db2fa985ca9861b895b058c789f8a24
Julia Friesel <julia.friesel@gmail.com>
2016-02-05 15:42:21 +0100

    delete unneeded stuff

--- t.js
+++
@@ -1,9 +0,0 @@
-var o = require('./index');
-var shift = o.keyCodes.XK_Shift_L;
-var f = o.MyObject();
-f.fakeButtonEvent(1, true, 3000);
-f.fakeButtonEvent(1, false, 0);
-f.fakeKeyEvent(shift, true, 0);
-    f.fakeKeyEvent(65, true, 0);
-    f.fakeKeyEvent(65, false, 0);
-f.fakeKeyEvent(shift, false, 0);
